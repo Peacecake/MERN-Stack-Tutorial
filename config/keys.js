16045402fea84db84751d096a8e7754ad476d210
Sebastian Peiser <peacecake@web.de>
2018-04-24 23:32:27 +0200

    Initial express server with routes

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://Admin:admin@ds111279.mlab.com:11279/devconnector'
+};
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profiles = require('./routes/api/profiles');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+//DB Config
+const db = require('./config/keys.js').mongoURI;
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (request, response) => response.send('Hello'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profiles', profiles);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running at port ${port}`));
